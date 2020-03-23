e45e959bcf8369f491096336805c4a0aadb7af6f
C00241011 <C00241011@louisiana.edu>
2020-03-23 00:16:34 -0500

    Jonathan: Added interative loading screen between scene 4 to scene 1.

---
+++ LoadingScreens/Assets/Scripts/ClickCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClickCounter : MonoBehaviour
+{
+	public Canvas canvas;
+	public TextMeshProUGUI text;
+	private int click;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		click = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		{
+			if (canvas.isActiveAndEnabled)
+			{
+				if (Input.GetMouseButtonDown(0))
+				{
+					click++;
+					text.text = "How Many Times Can You Click: " + click;
+				}
+			}
+		}
+	}
+}
