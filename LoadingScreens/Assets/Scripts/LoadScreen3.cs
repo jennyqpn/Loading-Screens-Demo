c45f93cff4f03b1738442fcd3c427325dc44a253
jwill925 <c00082940@louisiana.edu>
2020-03-23 20:24:40 -0500

    finished

---
+++ LoadingScreens/Assets/Scripts/LoadScreen3.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScreen3 : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Portal"))
+        {
+            FindObjectOfType<Screen3Loader>().LoadScene3(sceneToLoad);
+        }
+    }
+}
